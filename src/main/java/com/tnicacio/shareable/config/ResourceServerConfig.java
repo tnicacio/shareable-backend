dd1d406025f3b737d1477fcfd96e7d1fbcc362ea
tnicacio <nicacio.t92@gmail.com>
2021-06-29 18:55:37 -0300

    database seed

--- src/main/java/com/tnicacio/shareable/config/ResourceServerConfig.java
+++ src/main/java/com/tnicacio/shareable/config/ResourceServerConfig.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
@@ -16,11 +15,11 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	
-	private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**" };
-	
-	private static final String[] CLIENT = { "/users/**", "/sessions/**", "/knowledges/**" };
-
-	private static final String[] ADMIN = { "/roles/**" };
+//	private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**" };
+//	
+//	private static final String[] CLIENT = { "/users/**", "/sessions/**", "/knowledges/**" };
+//
+//	private static final String[] ADMIN = { "/roles/**" };
 	
 	
 	private Environment env;
